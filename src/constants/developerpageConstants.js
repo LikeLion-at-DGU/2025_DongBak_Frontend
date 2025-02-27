21cbec09c83051ba37d044e3af96b9f0cceab56f
taejun0 <xownswns@naver.com>
2025-02-28 00:35:06 +0900

    [Feat] #12 - 개발자페이지 UI 구현

---
+++ src/constants/developerpageConstants.js
@@ -0,0 +1,36 @@
+export const DEVELOPER_CONSTANT = {
+  SITE_EMOJI: "/images/developerPage/link.svg",
+  SITE_TEXT: "멋쟁이사자처럼 공식 사이트 바로가기",
+  TITLE: {
+    ABOUTUS: "images/developerPage/developers_title/aboutus.svg",
+    PD: "images/developerPage/developers_title/PD.svg",
+    FE: "images/developerPage/developers_title/FE.svg",
+    BE: "images/developerPage/developers_title/BE.svg",
+    EC: "images/developerPage/developers_title/EC.svg",
+  },
+}
+
+export const DEVELOPER_CARD_CONSTANT = {
+  ABOUTUS: 
+    ["안녕하세요, 동국대학교 멋쟁이사자처럼입니다.",
+      "동국대학교 내 모든 동아리를 알아볼 수 있는",
+      "동아리 박랍회를 위해여 '동아리랑' 사이트를 개발하였어요.",
+    ],
+  PD: [
+    { name: "전효준", major: "산업시스템공학과", role: ["PM", "DS"], image: "images/developerPage/PD1.svg"},
+    { name: "황민영", major: "경영정보학과", role: ["PM", "DS"], image: "images/developerPage/PD2.svg"},
+  ],
+  FE: [
+    { name: "박세호", major: "정보통신공학과", role: ["FE"], image: "images/developerPage/FE1.svg"},
+    { name: "오태준", major: "정보통신공학과", role: ["FE"], image: "images/developerPage/FE2.svg"},
+    { name: "하채민", major: "전자전기공학부", role: ["FE"], image: "images/developerPage/FE3.svg"},
+  ],
+  BE: [
+    { name: "김민기", major: "컴퓨터공학과", role: ["총괄", "BE"], image: "images/developerPage/BE1.svg"},
+    { name: "황채현", major: "전자전기공학부", role: ["BE"], image: "images/developerPage/BE2.svg"},
+  ],
+  EC: [
+    { name: "임수빈", major: "화공생물공학과", role: ["대외협력"], image: "images/developerPage/EC1.svg"},
+    { name: "임현우", major: "정보통신공학과", role: ["대외협력"], image: "images/developerPage/EC2.svg"},
+  ],
+}
