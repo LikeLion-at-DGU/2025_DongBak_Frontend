753ff35e41f797778ae1d71b51247e242d94bc93
taejun0 <xownswns@naver.com>
2025-02-28 00:55:20 +0900

    [Fix] #11 - 네브바 상단 relative 화

--- src/pages/mainpage/styled.js
+++ src/pages/mainpage/styled.js
@@ -5,4 +5,5 @@ export const Wrapper = styled.div`
   max-width: 540px;
   height: 100vh; /* 화면 높이에 맞춰 변경 */
   background: url("/images/mainBackImage.svg") no-repeat center / cover;
+  position: relative;
 `;
