9c5b48f198b9105f948ce7ffb55899994dea0891
sayyyho <323psh@naver.com>
2025-03-01 23:06:33 +0900

    [Feat] #8 - 공연페이지 타임라인 UI 구현

--- src/pages/performance/_components/container/PerformanceContainer.jsx
+++ src/pages/performance/_components/container/PerformanceContainer.jsx
@@ -1,12 +1,13 @@
 import * as S from "./styled";
+import { TODAY_PERFORMANCE_TITLE } from "@constants/performaceData";
 
 export const PerformanceContainer = ({ children }) => {
   return (
     <S.OuterWrapper>
-      <S.OuterWrapper>
-        <S.Title />
+      <S.InnerWrapper>
+        <S.Title>{TODAY_PERFORMANCE_TITLE}</S.Title>
         {children}
-      </S.OuterWrapper>
+      </S.InnerWrapper>
     </S.OuterWrapper>
   );
 };
